564d794fd8abf0cce841fb64d9a9c7d2a2a12fcc
adhithya11282006 <adhithyaparamasivam92@gmail.com>
2025-11-30 22:50:21 +0530

    Create expenseModel for managing expenses

---
+++ controllers/models/expenseModel.js
@@ -0,0 +1,13 @@
+// models/expenseModel.js
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  amount: { type: Number, required: true },
+  type: { type: String, enum: ["income", "expense"], required: true },
+  date: { type: Date, default: Date.now },
+  category: { type: String },
+  note: { type: String }
+});
+
+module.exports = mongoose.model("Expense", expenseSchema);
